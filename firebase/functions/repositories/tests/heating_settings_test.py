48468c44993cf369206279f6aaf7f522327da0be
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-06 22:51:16 +0200

    check all fields in heating settings test

--- firebase/functions/repositories/tests/heating_settings_test.py
+++ firebase/functions/repositories/tests/heating_settings_test.py
@@ -2,27 +2,38 @@
 from unittest import TestCase, main
 from mockfirestore import MockFirestore
 from repositories.heating_settings import get_heating_settings
+from repositories.firestore_collection_constants import HEATING_SETTINGS_COLLECTION
 
 class HeatingSettingsTest(TestCase):
     """Unit tests for the heating settings repository."""
     def test_get_heating_settings(self):
-        """Test that heating settings are fetched correctly from Firestore."""
+        """Test that heating settings are fetched correctly from Firestore 
+        and all the needed fields are populated."""
+
         # Arrange
+        key = '123'
         mock_firestore_client = MockFirestore()
-        mock_firestore_client.collection('HeatingSettings').document('123').set({
-            'maxPrice': 0,
-            'lowPrice': 0,
+        mock_firestore_client.collection(HEATING_SETTINGS_COLLECTION).document(key).set({
+            'maxPrice': 10,
+            'lowPrice': 5,
             'heatingEnabled': True,
-            'heatingMaxTemperature': 0,
-            'heatingMinTemperature': 0,
-            'heatingMidTemperature': 0
+            'heatingMaxTemperature': 20,
+            'heatingMinTemperature': 5,
+            'heatingMidTemperature': 10
         })
+
         # Act
-        settings_map = get_heating_settings(mock_firestore_client, {'123'})
+        settings_map = get_heating_settings(mock_firestore_client, {key})
 
         # Assert
-        self.assertTrue('123' in settings_map)
-        self.assertEqual(settings_map['123'].heating_enabled, True)
+        self.assertTrue(key in settings_map)
+        settings = settings_map[key]
+        self.assertEqual(settings.heating_enabled, True)
+        self.assertEqual(settings.max_price, 10)
+        self.assertEqual(settings.low_price, 5)
+        self.assertEqual(settings.heating_max_temperature, 20)
+        self.assertEqual(settings.heating_min_temperature, 5)
+        self.assertEqual(settings.heating_mid_temperature, 10)
 
 
 if __name__ == "__main__":
