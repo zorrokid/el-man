b8b0f28874b188ec6dde2e795bc2d941e93c0f52
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-11-06 23:29:12 +0200

    fix test runner script

--- firebase/functions/run_tests.sh
+++ firebase/functions/run_tests.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 python -m unittest domain/tests/target_temprerature_test.py
 python -m unittest models/tests/price_test.py
 python -m unittest repositories/tests/heating_settings_test.py 
