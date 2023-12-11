e1e9e058eb58fe3e2ea6c22446d1fde6198791af
Mikko Keinänen <mikko.keinanen@gmail.com>
2023-12-11 21:03:39 +0200

    Add more unit tests for temperature management

--- firebase/functions/domain/tests/target_temprerature_test.py
+++ firebase/functions/domain/tests/target_temprerature_test.py
@@ -8,7 +8,8 @@ class TestTargetTemperature(unittest.TestCase):
     """Unit tests for the target temperature calculation"""
 
     def test_target_temperature_price_is_none_and_temperature_above_min_temperature(self):
-        """Test that target temperature is zero and heating is disabled when price is None"""
+        """Test that target temperature is zero and heating is disabled when price is None
+        and temperature is above min temperature"""
         price = None
         settings = get_default_heating_settings()
         current_temperature = 10.0
@@ -17,7 +18,8 @@ class TestTargetTemperature(unittest.TestCase):
         self.assertEqual(0, target_temperature)
 
     def test_target_temperature_price_is_none_and_temperature_below_min_temperature(self):
-        """Test that target temperature is zero and heating is disabled when price is None"""
+        """Test that target temperature is min temperature and heating is enabled when price is None
+        and temperature is below min temperature"""
         price = None
         settings = get_default_heating_settings()
         current_temperature = 1.0
@@ -60,8 +62,8 @@ class TestTargetTemperature(unittest.TestCase):
         self.assertEqual(heating_enabled, False)
         self.assertEqual(target_temperature, 0)
 
-    def test_target_temperature_price_exceed_max(self):
-        """Test that target temperature is minimum temperature when price exceeds max price"""
+    def test_target_temperature_price_exceed_max_and_temperature_below_min(self):
+        """Test that target temperature is minimum temperature when price exceeds max price and temperature is below min temperature"""
         price = 20.0
         settings = HeatingSettings(
             max_price=10.0,
@@ -69,13 +71,30 @@ class TestTargetTemperature(unittest.TestCase):
             heating_enabled=True,
             heating_max_temperature=20.0,
             heating_mid_temperature=15.0,
-            heating_min_temperature=10.0
+            heating_min_temperature=5.0
         )
-        current_temperature = 10.0
+        current_temperature = 4.0
 
         (heating_enabled, target_temperature) = calculate_target_temperature(price, settings, current_temperature)
         self.assertEqual(heating_enabled, True)
         self.assertEqual(target_temperature, settings.heating_min_temperature)
+
+    def test_target_temperature_price_exceed_max_and_temperature_above_min(self):
+        """Test that target temperature is minimum temperature when price exceeds max price and temperature is abobe min temperature"""
+        price = 20.0
+        settings = HeatingSettings(
+            max_price=10.0,
+            low_price=5.0,
+            heating_enabled=True,
+            heating_max_temperature=20.0,
+            heating_mid_temperature=15.0,
+            heating_min_temperature=5.0
+        )
+        current_temperature = 10.0
+
+        (heating_enabled, target_temperature) = calculate_target_temperature(price, settings, current_temperature)
+        self.assertEqual(heating_enabled, False)
+        self.assertEqual(target_temperature, 0)
 
     def test_target_temperature_price_below_max(self):
         """Test that target temperature is mid temperature and 
